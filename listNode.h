c18cb72fd55fc9ec8a7d6c9eb8593c0558259bd0
NixBoss-arch <76533731+NixBoss-arch@users.noreply.github.com>
2024-10-29 13:14:31 -0500

    Update listNode.h

--- listNode.h
+++ listNode.h
@@ -6,12 +6,20 @@
 
 #ifndef LISTNODE_H
 #define LISTNODE_H
+<<<<<<< Updated upstream
 #include <iostream>
 
 template <typename T>
 class listNode{
     private:
     //creating what's needed for a linked list
+=======
+
+template <typename foodGroup>
+class listNode{
+    private:
+    //creating the node value and next node pointer 
+>>>>>>> Stashed changes
         T obj;
         listNode* pnt;
 
@@ -22,13 +30,21 @@ class listNode{
         }
 
         //Overloaded constructor
+<<<<<<< Updated upstream
         listNode(T o){
+=======
+        listNode(foodGroup o){
+>>>>>>> Stashed changes
             obj = o;
             pnt = NULL;
         }
 
         //Another Overload
+<<<<<<< Updated upstream
         listNode(T o, listNode* p){
+=======
+        listNode(foodGroup o, listNode* p){
+>>>>>>> Stashed changes
             obj = o;
             pnt = p;
         }
@@ -54,12 +70,20 @@ class listNode{
         //Mutators
 
         //sets data in node
+<<<<<<< Updated upstream
         void setObj(T obj){
+=======
+        void setObj(foodGroup obj){
+>>>>>>> Stashed changes
             this->obj = obj;
         }
 
         //sets pointer to next node
+<<<<<<< Updated upstream
         void setPnt(listNode<T>* pnt){
+=======
+        void setPnt(listNode* pnt){
+>>>>>>> Stashed changes
             this->pnt = pnt;
         }
 };
